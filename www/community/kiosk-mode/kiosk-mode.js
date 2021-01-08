98085d4e8203c94af7c3e7113a195c572fcafa7c
uherting <uherting@users.noreply.github.com>
2021-01-08 13:12:49 +0100

    update HACS

--- www/community/kiosk-mode/kiosk-mode.js
+++ www/community/kiosk-mode/kiosk-mode.js
@@ -1,48 +1,42 @@
-"use strict";function e(e,t){return o(e)||n(e,t)||d(e,t)||r()}function r(){throw new TypeError(
+"use strict";function e(e,t){return o(e)||r(e,t)||c(e,t)||n()}function n(){throw new TypeError(
 "Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}
-function n(e,t){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e)){var r=[],n=!0,o=!1,a=void 0;try{for(var i,s=e[Symbol.iterator]();!(n=(
-i=s.next()).done)&&(r.push(i.value),!t||r.length!==t);n=!0);}catch(e){o=!0,a=e}finally{try{n||null==s.return||s.return()}finally{if(o)throw a}}
-return r}}function o(e){if(Array.isArray(e))return e}function t(e){return s(e)||i(e)||d(e)||a()}function a(){throw new TypeError(
+function r(e,t){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e)){var n=[],r=!0,o=!1,a=void 0;try{for(var i,s=e[Symbol.iterator]();!(r=(
+i=s.next()).done)&&(n.push(i.value),!t||n.length!==t);r=!0);}catch(e){o=!0,a=e}finally{try{r||null==s.return||s.return()}finally{if(o)throw a}}
+return n}}function o(e){if(Array.isArray(e))return e}function t(e){return s(e)||i(e)||c(e)||a()}function a(){throw new TypeError(
 "Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function i(e){
-if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e))return Array.from(e)}function s(e){if(Array.isArray(e))return l(e)}function v(e,t){var r
-;if("undefined"==typeof Symbol||null==e[Symbol.iterator]){if(Array.isArray(e)||(r=d(e))||t&&e&&"number"==typeof e.length){r&&(e=r);var n=0,t=function(
-){};return{s:t,n:function(){return n>=e.length?{done:!0}:{done:!1,value:e[n++]}},e:function(e){throw e},f:t}}throw new TypeError(
+if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e))return Array.from(e)}function s(e){if(Array.isArray(e))return d(e)}function v(e,t){var n
+;if("undefined"==typeof Symbol||null==e[Symbol.iterator]){if(Array.isArray(e)||(n=c(e))||t&&e&&"number"==typeof e.length){n&&(e=n);var r=0,t=function(
+){};return{s:t,n:function(){return r>=e.length?{done:!0}:{done:!1,value:e[r++]}},e:function(e){throw e},f:t}}throw new TypeError(
 "Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var o,a=!0,
-i=!1;return{s:function(){r=e[Symbol.iterator]()},n:function(){var e=r.next();return a=e.done,e},e:function(e){i=!0,o=e},f:function(){try{
-a||null==r.return||r.return()}finally{if(i)throw o}}}}function d(e,t){if(e){if("string"==typeof e)return l(e,t);var r=Object.prototype.toString.call(e
-).slice(8,-1);return"Object"===r&&e.constructor&&(r=e.constructor.name),"Map"===r||"Set"===r?Array.from(e
-):"Arguments"===r||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)?l(e,t):void 0}}function l(e,t){(null==t||t>e.length)&&(t=e.length);for(var r=0,
-n=new Array(t);r<t;r++)n[r]=e[r];return n}var m=document.querySelector("home-assistant"),k=m.shadowRoot.querySelector("home-assistant-main"
-).shadowRoot,c=k.querySelector("partial-panel-resolver"),p=k.querySelector("app-drawer-layout");window.kiosk_entities=[];var u,w=0,b={};function h(){
-w++;try{var e=u.lovelace.config;b=e.kiosk_mode||{},y()}catch(e){w<40&&setTimeout(function(){return h()},50)}}function _(e){
-var t=window.location.search;return e.some(function(e){return t.includes(e)})}function S(e){return e&&!e.querySelector("#kiosk_mode")}function g(e,t){
-var r=document.createElement("style"),n="app-toolbar"==t.localName?"kiosk_mode_menu":"kiosk_mode";r.setAttribute("id",n),r.innerHTML=e,t.appendChild(r
-),window.dispatchEvent(new Event("resize"))}function x(e,t){window.localStorage.setItem(e,t)}function A(e){return"true"==window.localStorage.getItem(e
-)}function f(){u=k.querySelector("ha-panel-lovelace"),!window.location.search.includes("disable_km")&&u&&h()}function y(){var t=m.hass;w=0;var e=A(
-"kmHeader")||_(["kiosk","hide_header"]),r=A("kmSidebar")||_(["kiosk","hide_sidebar"]),n=b.admin_settings,o=b.non_admin_settings,a=b.user_settings,
-i=b.entity_settings,s=r||e,e=s?e:b.kiosk||b.hide_header,r=s?r:b.kiosk||b.hide_sidebar;if(n&&t.user.is_admin&&(e=n.kiosk||n.hide_header,
-r=n.kiosk||n.hide_sidebar),o&&!t.user.is_admin&&(e=o.kiosk||o.hide_header,r=o.kiosk||o.hide_sidebar),a){Array.isArray(a)||(a=[a]);var d=v(a);try{for(
-d.s();!(c=d.n()).done;){var l=c.value,c=l.users;Array.isArray(l.users)||(c=[c]),c.some(function(e){return e.toLowerCase()==t.user.name.toLowerCase()}
-)&&(e=l.kiosk||l.hide_header,r=l.kiosk||l.hide_sidebar)}}catch(e){d.e(e)}finally{d.f()}}if(i){var u=v(i);try{for(u.s();!(y=u.n()).done;){var h=y.value
-,f=Object.keys(h.entity)[0];window.kiosk_entities.includes(f)||window.kiosk_entities.push(f);var y=h.entity[f];t.states[f].state==y&&("kiosk"in h?(
-e=h.kiosk,r=h.kiosk):("hide_header"in h&&(e=h.hide_header),"hide_sidebar"in h&&(r=h.hide_sidebar)))}}catch(e){u.e(e)}finally{u.f()}}a=k.querySelector(
-"ha-panel-lovelace"),i=a?a.shadowRoot.querySelector("hui-root").shadowRoot:null,a=i?i.querySelector("app-toolbar"):null;(r||e)&&(e&&S(i)&&(g(
-"#view { min-height: 100vh !important } app-header { display: none }",i),url.includes("cache")&&x("kmHeader","true")),r&&S(p)&&(g(
-":host { --app-drawer-width: 0 !important } #drawer { display: none }",p),S(a)&&g("ha-menu-button { display:none !important } ",a),url.includes(
-"cache")&&x("kmSidebar","true"))),e||S(i)||i.querySelector("#kiosk_mode").remove(),r||S(p)||p.querySelector("#kiosk_mode").remove(),
-!r&&a.querySelector("#kiosk_mode_menu")&&a.querySelector("#kiosk_mode_menu").remove(),window.dispatchEvent(new Event("resize"))}function q(e){var t,
-r=v(e);try{for(r.s();!(t=r.n()).done;){var n=v(t.value.addedNodes);try{for(n.s();!(o=n.n()).done;){var o=o.value;if("ha-panel-lovelace"==o.localName
-)return void new MutationObserver(E).observe(o.shadowRoot,{childList:!0})}}catch(e){n.e(e)}finally{n.f()}}}catch(e){r.e(e)}finally{r.f()}}function E(e
-){var t,r=v(e);try{for(r.s();!(t=r.n()).done;){var n=v(t.value.addedNodes);try{for(n.s();!(o=n.n()).done;){var o=o.value;if("hui-root"==o.localName
-)return void new MutationObserver(O).observe(o.shadowRoot,{childList:!0})}}catch(e){n.e(e)}finally{n.f()}}}catch(e){r.e(e)}finally{r.f()}}function O(e
-){var t,r=v(e);try{for(r.s();!(t=r.n()).done;){var n,o=v(t.value.addedNodes);try{for(o.s();!(n=o.n()).done;)if("ha-app-layout"==n.value.localName
-)return window.kiosk_entities=[],b={},void f()}catch(e){o.e(e)}finally{o.f()}}}catch(e){r.e(e)}finally{r.f()}}window.location.search.includes(
-"clear_km_cache")&&["kmHeader","kmSidebar"].forEach(function(e){return x(e,"false")}),f(),new MutationObserver(q).observe(c,{childList:!0}),
-window.hassConnection.then(function(e){return e.conn.socket.onmessage=function(e){window.kiosk_entities.length<1||(e=JSON.parse(e.data).event
-)&&"state_changed"==e.event_type&&e.data.new_state.state!=e.data.old_state.state&&window.kiosk_entities.includes(e.data.entity_id)&&(b={},f())}});for(
-var j={header:"%c≡ kiosk-mode".padEnd(27),ver:"%cversion 1.5.4 "},I="%c\n",N=Math.max.apply(Math,t(Object.values(j).map(function(e){return e.length}))
-),M=0,L=Object.entries(j);M<L.length;M++){var R=e(L[M],1),C=R[0];j[C].length<=N&&(j[C]=j[C].padEnd(N)),"header"==C&&(j[C]="".concat(j[C].slice(0,-1),
-"⋮ "))}
-var T="display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;"
-,H="border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";console.info(j.header+I+j.ver,T,"","".concat(T," "
-).concat(H));
+i=!1;return{s:function(){n=e[Symbol.iterator]()},n:function(){var e=n.next();return a=e.done,e},e:function(e){i=!0,o=e},f:function(){try{
+a||null==n.return||n.return()}finally{if(i)throw o}}}}function c(e,t){if(e){if("string"==typeof e)return d(e,t);var n=Object.prototype.toString.call(e
+).slice(8,-1);return"Object"===n&&e.constructor&&(n=e.constructor.name),"Map"===n||"Set"===n?Array.from(e
+):"Arguments"===n||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?d(e,t):void 0}}function d(e,t){(null==t||t>e.length)&&(t=e.length);for(var n=0,
+r=new Array(t);n<t;n++)r[n]=e[n];return r}var b=document.querySelector("home-assistant"),u=b.shadowRoot.querySelector("home-assistant-main"
+).shadowRoot,l=u.querySelector("partial-panel-resolver"),k=u.querySelector("app-drawer-layout"),w=0;function h(){var e=u.querySelector(
+"ha-panel-lovelace");!g("disable_km")&&e&&f(e)}function f(t){w++;try{var e=t.lovelace.config.kiosk_mode||{};m(t,e)}catch(e){w<40&&setTimeout(function(
+){return f(t)},50)}}function _(e){return Array.isArray(e)?e:[e]}function g(e){return _(e).some(function(e){return window.location.search.includes(e)})
+}function S(e,t){window.localStorage.setItem(e,t)}function x(e){return"true"==window.localStorage.getItem(e)}function y(e){return e.querySelector(
+"#kiosk_mode_".concat(e.localName))}function A(e,t){var n;y(t)||((n=document.createElement("style")).setAttribute("id","kiosk_mode_".concat(
+t.localName)),n.innerHTML=e,t.appendChild(n))}function j(e){_(e).forEach(function(e){y(e)&&e.querySelector("#kiosk_mode_".concat(e.localName)).remove(
+)})}function m(e,t){w=0;var n=b.hass,r=e.shadowRoot.querySelector("hui-root").shadowRoot,o=r.querySelector("app-toolbar"),a=t.admin_settings,
+i=t.non_admin_settings,s=t.entity_settings,c=t.user_settings,d=x("kmHeader")||g(["kiosk","hide_header"]),e=(u=x("kmSidebar")||g(["kiosk",
+"hide_sidebar"]))||d,d=e?d:t.kiosk||t.hide_header,u=e?u:t.kiosk||t.hide_sidebar;if(a&&n.user.is_admin&&(d=a.kiosk||a.hide_header,
+u=a.kiosk||a.hide_sidebar),i&&!n.user.is_admin&&(d=i.kiosk||i.hide_header,u=i.kiosk||i.hide_sidebar),s){window.kiosk_entities=[];var l=v(s);try{for(
+l.s();!(y=l.n()).done;){var h=y.value,f=Object.keys(h.entity)[0],y=h.entity[f];window.kiosk_entities.push(f),n.states[f].state==y&&(
+"hide_header"in h&&(d=h.hide_header),"hide_sidebar"in h&&(u=h.hide_sidebar),"kiosk"in h&&(d=u=h.kiosk))}}catch(e){l.e(e)}finally{l.f()}}if(c){var m=v(
+_(c));try{for(m.s();!(p=m.n()).done;){var p=p.value;_(p.users).some(function(e){return e.toLowerCase()==n.user.name.toLowerCase()})&&(
+d=p.kiosk||p.hide_header,u=p.kiosk||p.hide_sidebar)}}catch(e){m.e(e)}finally{m.f()}}d?(A("#view{min-height:100vh !important}app-header{display:none}",
+r),g("cache")&&S("kmHeader","true")):j(r),u?(A(":host{--app-drawer-width:0 !important}#drawer{display:none}",k),A(
+"ha-menu-button{display:none !important}",o),g("cache")&&S("kmSidebar","true")):j([o,k]),window.dispatchEvent(new Event("resize"))}function p(e){q(e,
+"ha-panel-lovelace",E)}function E(e){q(e,"hui-root",O)}function O(e){q(e,"ha-app-layout",null)}function q(e,t,n){var r,o=v(e);try{for(o.s();!(r=o.n()
+).done;){var a=v(r.value.addedNodes);try{for(a.s();!(i=a.n()).done;){var i=i.value;if(i.localName==t)return void(n?new MutationObserver(n).observe(
+i.shadowRoot,{childList:!0}):h())}}catch(e){a.e(e)}finally{a.f()}}}catch(e){o.e(e)}finally{o.f()}}g("clear_km_cache")&&["kmHeader","kmSidebar"
+].forEach(function(e){return S(e,"false")}),h(),window.hassConnection.then(function(e){return e.conn.socket.onmessage=function(e){
+window.kiosk_entities.length<1||(e=JSON.parse(e.data).event)&&"state_changed"==e.event_type&&window.kiosk_entities.includes(e.data.entity_id
+)&&e.data.new_state.state!=e.data.old_state.state&&h()}}),new MutationObserver(p).observe(l,{childList:!0});for(var I={header:"%c≡ kiosk-mode".padEnd(
+27),ver:"%cversion 1.5.6 "},M="%c\n",N=Math.max.apply(Math,t(Object.values(I).map(function(e){return e.length}))),C=0,L=Object.entries(I
+);C<L.length;C++){var R=e(L[C],1),T=R[0];I[T].length<=N&&(I[T]=I[T].padEnd(N)),"header"==T&&(I[T]="".concat(I[T].slice(0,-1),"⋮ "))}
+var H="display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;"
+,z="border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";console.info(I.header+M+I.ver,H,"","".concat(H," "
+).concat(z));
