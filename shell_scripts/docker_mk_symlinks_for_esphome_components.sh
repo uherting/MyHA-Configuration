3f1bb83812a655949be5521e2c0da39626752f29
uherting <uherting@users.noreply.github.com>
2021-02-20 23:06:27 +0100

    added READMEs and blank script creating symlinks to all dirs located in esphome/custom_components_dev

---
+++ shell_scripts/docker_mk_symlinks_for_esphome_components.sh
@@ -0,0 +1,14 @@
+# purpose: create symlinks to all directories located in the MyHAconfiguration
+# repository under esphome/custom_components_dev
+
+# more information
+# Directory locations in the docker container
+# -------------------------------------------
+# - MyHAconfiguration repository: esphome/custom_components_dev
+# - docker container: /..../components
+#     - this is the dir which needs to contain symlinks to all dirs located
+#         in esphome/custom_components_dev
+#     - see esphome/custom_components_dev/../../shell_scripts for the script
+#         docker_mk_symlinks_for_esphome_components.sh
+    
+# EOF
