738d9f4839fd0fccf9df1505cf51d6b9bf05b8c8
uherting <uherting@gmx.de>
2021-07-14 15:42:50 +0200

    debugged shell script backup_secrets_file.sh

--- shell_scripts/backup_secrets_file.sh
+++ shell_scripts/backup_secrets_file.sh
@@ -14,5 +14,6 @@ fi
 
 
 TS=`date +%Y%m%d_%H%M%S`
-scp /usr/share/hassio/homeassistant/esphome/secrets.yaml uwe@beta.local:/home/uwe/Desktop/HA/secrets_${TS}.yaml
+# scp /usr/share/hassio/homeassistant/esphome/secrets.yaml uwe@beta.local:/home/uwe/Desktop/HA/secrets_${TS}.yaml
+scp /usr/share/hassio/homeassistant/esphome/secrets.yaml uwe@192.168.178.32:/home/uwe/Desktop/HA/secrets_${TS}.yaml
 
