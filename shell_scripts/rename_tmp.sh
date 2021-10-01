7b103c40740e2f6cd1a3ee9d987244492eb59647
uherting <uherting@users.noreply.github.com>
2021-10-01 18:55:19 +0200

    snapshot prior renaming xiami sensors

---
+++ shell_scripts/rename_tmp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+for i in `find .. -type f -name \*.yaml | xargs grep \
+      -e sensor.xiaomi_bathroom \
+      -e sensor.xiaomi_bedroom \
+      -e sensor.xiaomi_lounge \
+      -e sensor.xiaomi_storage`
+do
+  echo $i
+  sed -i \
+      -e "s/sensor.xiaomi_bathroom/sensor.xiaomi_l2_bathroom/g" \
+      -e "s/sensor.xiaomi_bedroom/sensor.xiaomi_l2_bedroom/g" \
+      -e "s/sensor.xiaomi_lounge/sensor.xiaomi_l2_lounge/g" \
+      -e "s/sensor.xiaomi_storage/sensor.xiaomi_l2_storage/g"
+done
