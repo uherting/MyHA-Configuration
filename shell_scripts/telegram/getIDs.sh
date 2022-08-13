4a435b4fe9f6e95abb87507f7aa74478aac71937
uherting <uherting@users.noreply.github.com>
2022-08-13 12:49:33 +0200

    added shell script to determine telegram id of bot or group

---
+++ shell_scripts/telegram/getIDs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this script is based on what is to be found at https://www.home-assistant.io/integrations/telegram
+# it can help determine the id of the bot or a group
+
+# usage: 
+#   1.) send msg to bot or group
+#   2.) execute this script
+
+
+BNAME=`basename $0 .sh`
+DNAME=`dirname $0`
+SECRETS_FILE="${DNAME}/../../secrets.yaml"
+
+MY_API_TOKEN=`grep TELEGRAM_TOKEN ${SECRETS_FILE} | cut -f2 -d'"'`
+curl -X GET https://api.telegram.org/bot${MY_API_TOKEN}/getUpdates
