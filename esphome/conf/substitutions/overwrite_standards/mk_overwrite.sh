8786f5c1d9de1eba21ac50dc3280bf6d4169a2a3
uherting <uherting@users.noreply.github.com>
2022-10-15 22:17:26 +0200

    debugging

--- esphome/conf/substitutions/overwrite_standards/mk_overwrite.sh
+++ esphome/conf/substitutions/overwrite_standards/mk_overwrite.sh
@@ -1,10 +1,11 @@
 TGT_DIR="overwrites"
-for i in l*yaml
+for i in `ls l*yaml | grep -v -e ttgo -e cam32`
 do
   TGT_FILE="${TGT_DIR}/${i}"
   echo "####################################################################" > ${TGT_FILE}
   echo "# OVERWRITE COMMON SUBSTITUTIONS" >> ${TGT_FILE}
   echo "" >> ${TGT_FILE}
   
-  grep dht01_upd ${i} >> ${TGT_FILE}
+  grep dht01_upd ${i} | grep -v DHT_UPDATE_INTERVAL_15MIN >> ${TGT_FILE}
+  grep Door ${i} >> ${TGT_FILE}
 done
