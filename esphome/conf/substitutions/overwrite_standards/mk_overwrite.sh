72e4573c037ea5be1cc4c1c463a8d2084adc2610
uherting <uherting@users.noreply.github.com>
2022-10-15 21:14:56 +0200

    added working files

---
+++ esphome/conf/substitutions/overwrite_standards/mk_overwrite.sh
@@ -0,0 +1,10 @@
+TGT_DIR="overwrites"
+for l*yaml
+do
+  TGT_FILE="${TGT_DIR}/${i}"
+  echo "####################################################################" > ${TGT_FILE}
+  echo "# OVERWRITE COMMON SUBSTITUTIONS" >> ${TGT_FILE}
+  echo "" >> ${TGT_FILE}
+  
+  grep dht01_upd ${i} >> ${TGT_FILE}
+done
