28ad066cabd01502641f2bd318a3c60c3e17dd1e
uherting <uherting@users.noreply.github.com>
2024-11-16 19:38:02 +0100

    debugged shell scripts

--- shell_scripts/telegram/getIDs.sh
+++ shell_scripts/telegram/getIDs.sh
@@ -15,3 +15,5 @@ SECRETS_FILE="${DNAME}/../../secrets.yaml"
 MY_API_TOKEN=`grep TELEGRAM_TOKEN ${SECRETS_FILE} | cut -f2 -d'"'`
 echo "The token is: ${MY_API_TOKEN}"
 curl -X GET https://api.telegram.org/bot${MY_API_TOKEN}/getUpdates
+echo " "
+
