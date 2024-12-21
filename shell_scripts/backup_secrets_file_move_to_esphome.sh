b24cdb3d3228e7b229e19e086fdd6b2451a6e27a
uherting <uherting@users.noreply.github.com>
2024-12-21 14:24:14 +0100

    debugged move script

--- shell_scripts/backup_secrets_file_move_to_esphome.sh
+++ shell_scripts/backup_secrets_file_move_to_esphome.sh
@@ -2,8 +2,8 @@
 
 ##########################################################################################
 #
-#  this script takes the secrets.yaml file from its backup location on on localhost 
-#   and places it into the repository dir to be used on beta
+#  this script takes the secrets.yaml file from its backup location on host winnipeg
+#   and places it into the repository dir to be used on winnipeg
 #
 ##########################################################################################
 
@@ -17,11 +17,14 @@ TS=`date +%Y%m%d_%H%M%S`
 
 HOST_NAME=`hostname`
 
-if [ "$HOST_NAME" == "beta" ];then
+if [ "$HOST_NAME" == "winnipeg" ];then
   HOME_DIR="/home/uwe"
   HOME_REPOSITORY="/Git/UH/MyHAConfig"
 
   SRC_FILE=`ls -rt ${HOME_DIR}/Desktop/secrets_*yaml | tail -n1`
   TGT_FILE="${HOME_DIR}${HOME_REPOSITORY}/esphome/secrets.yaml"
+
+  echo "Using ${SRC_FILE} for copying content to ${TGT_FILE}"
+
   cat $SRC_FILE > $TGT_FILE
 fi
