f516412cd1ec773b0bd98ed5fabf88fc61aa878b
uherting <uherting@users.noreply.github.com>
2025-03-14 19:43:56 +0100

    update HACS

--- custom_components/metnowcast/met_api.py
+++ custom_components/metnowcast/met_api.py
@@ -3,14 +3,13 @@ from __future__ import annotations
 from datetime import datetime
 import logging
 import requests
-from requests_oauthlib import OAuth2Session
-from .const import NotFound
+from .const import NotFound, VERSION
 
 _LOGGER = logging.getLogger(__name__)
 
-BASE_URL = "https://api.met.no/weatherapi/nowcast/2.0"
+BASE_URL = "https://aa015h6buqvih86i1.api.met.no/weatherapi/nowcast/2.0"
 REQUEST_HEADER = {
-    "User-Agent": "home-assistant-metnowcast https://github.com/toringer/home-assistant-metnowcast"
+    "User-Agent": f"home-assistant-metnowcast/{VERSION} https://github.com/toringer/home-assistant-metnowcast"
 }
 
 
@@ -19,14 +18,13 @@ class MetApi:
 
     def __init__(self) -> None:
         """Init"""
-        self._session: OAuth2Session = None
-        self._session_expires_at: datetime = datetime.now()
 
     def get_complete(self, lat: float, lon: float):
         """Get complete forecast"""
         url = f"{BASE_URL}/complete"
         param = {"lat": lat, "lon": lon}
         response = requests.get(url=url, params=param, headers=REQUEST_HEADER)
+        _LOGGER.debug(f"REQUEST_HEADER: {REQUEST_HEADER}")
         if response.status_code != 200:
             raise NotFound
 
