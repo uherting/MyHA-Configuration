df203683e49ea145e2eea53a882bd1c8ad80ecf6
uherting <uherting@users.noreply.github.com>
2025-06-12 16:33:12 +0200

    ESPHoem dashboard start script: added USB device for ESP32C3

--- shell_scripts/docker_image_esphome_dashboard_lcl_start.sh
+++ shell_scripts/docker_image_esphome_dashboard_lcl_start.sh
@@ -9,12 +9,19 @@ fi
 . ${DNAME}/scriptConfig
 # echo "BUILD_DIR_FOR_DOCKER=${BUILD_DIR_FOR_DOCKER}"
 
-USB_TO_TEST="/dev/ttyUSB0"
+USB_TO_TEST="/dev/ttyUSB0 /dev/ttyACM0"
 ADDITIONAL_USB=""
-if [ -e ${USB_TO_TEST} ]; then
-  ADDITIONAL_USB="--device=${USB_TO_TEST}"
+
+for USB in ${USB_TO_TEST}; do
+  if [ -e ${USB} ]; then
+    ADDITIONAL_USB="${ADDITIONAL_USB} --device=${USB}"
+  fi
+done
+
+if [ "${ADDITIONAL_USB}" == "" ]; then
+  echo "No USB devices found. ESPHome dashboard will not be able to connect to any devices."
 else
-  echo "No device on ${USB_TO_TEST}. Starting dashboard without USB device."
+  echo "Found USB devices: ${ADDITIONAL_USB}"
 fi
 
 # create a container and start it. if the container is stopped it will be removed (--rm)
