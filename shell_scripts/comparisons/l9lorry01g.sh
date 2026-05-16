06ad09ab6f31e4cb0fb0188b6d1eed86198a66fb
uherting <uherting@users.noreply.github.com>
2026-05-16 15:35:25 +0200

    added actual comparison

--- shell_scripts/comparisons/l9lorry01g.sh
+++ shell_scripts/comparisons/l9lorry01g.sh
@@ -12,5 +12,20 @@ if [ "$DNAME" == "." ];then
  DNAME=$(pwd)
 fi
 
-. ${DNAME}/scriptConfig
+. ${DNAME}/../scriptConfig
 
+cd ${ESPHOME_PATH}
+if [ $? -ne 0 ]; then
+  echo "Cannot cd to ${ESPHOME_PATH}. Exiting."
+  exit 1
+fi
+
+for file in $(find . -name ${BNAME}.yaml)
+do
+  DN=$(dirname $file)
+  CMP_FILE=${DN}/l2kitchen12.yaml
+  echo "Comparing files ... ======================================"
+  echo "${file}"
+  echo "${CMP_FILE}"
+  diff -u $file ${CMP_FILE}
+done
